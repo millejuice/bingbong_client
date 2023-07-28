0e634466d2d3242ade480be8b8442a871acba383
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2023-07-28 21:15:47 +0900

    [feat]: 유사성 검증 API (#1)

---
+++ src/nlp/similarityCheck.js
@@ -0,0 +1,23 @@
+async function query(data) {
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2",
+        {
+            headers: { Authorization: "Bearer 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" },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+}
+
+query({"inputs": {
+        "source_sentence": "That is a happy person",
+        "sentences": [
+            "That is a happy dog",
+            "That is a very happy person",
+            "Today is a sunny day"
+        ]
+    }}).then((response) => {
+    console.log(JSON.stringify(response));
+});
