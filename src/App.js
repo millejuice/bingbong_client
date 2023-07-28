cc89a27afe411861bed9fa865707c9065360a120
millejuice <potgusdavy@gmail.com>
2023-07-29 03:27:38 +0900

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import background from "./image/background.png"
 import Chat from './page/chat';
 import Login from './page/login';
 import { Box } from '@mui/material';
-import AlertDialog from './component/alert';
+import AlertDialog from './component/chatRoom.js';
 
 function App() {
   return (
