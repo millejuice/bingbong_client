805c01297335db0776e83f3d57e1cc5e20f2f343
millejuice <potgusdavy@gmail.com>
2023-07-29 05:29:19 +0900

    sidebar

--- src/component/sidebar.js
+++ src/component/sidebar.js
@@ -2,22 +2,38 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import walden from '../image/walden.png'
-import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import SettingsIcon from '@mui/icons-material/Settings';
+import homeLogo from '../image/homeLogo.png';
+import firstIcon from '../image/1icon.png';
+import secondIcon from '../image/2icon.png';
+import thirdIcon from '../image/3icon.png';
 
 const Sidebar = () => {
   return (
-    <Box sx={{ width: 82, backgroundColor: '#f0f0f0', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', padding: '20px' }}>
-      <img src={walden} alt="Logo" style={{ width: '52px', height: '52px', marginBottom: '20px' }} />
-      <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-      <PeopleOutlineIcon />
+    <Box
+      sx={{
+        width: 82,
+        backgroundColor: "#f0f0f0",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        padding: "20px",
+      }}
+    >
+      <img
+        src={homeLogo}
+        alt="Logo"
+        style={{ width: "52px", height: "52px", marginBottom: "20px" }}
+      />
+      <Box sx={{ display: "flex", alignItems: "center", paddingTop : '51px' }}>
+        <img src={firstIcon} />
       </Box>
-      <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-      <ChatBubbleOutlineIcon />
+      <Box sx={{ display: "flex", alignItems: "center", paddingTop : '28px' }}>
+        <img src={secondIcon} />
       </Box>
-      <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-      <SettingsIcon />
+      <Box sx={{ display: "flex", alignItems: "center", paddingTop : '30px'}}>
+        <img src={thirdIcon} />
       </Box>
     </Box>
   );
