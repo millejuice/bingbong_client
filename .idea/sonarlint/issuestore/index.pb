b71d8fbdd0432b6d0ad5aa918128e8ffeb6a5ad0
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2023-07-29 09:29:24 +0900

    Yujin (#21)
    * [feat]: 유사성 검증 API
    
    * [feat]: API 요청 코드 (to be fixed)
    
    * temp
    
    * [feat]: 채팅 종료 alert
    
    * [feat]: 프로필, AI분석결과 개발
    
    * [feat]: 프로필, AI분석결과 개발
    
    * [style]: 색 조정
    
    * [style]: 화질 조정
    
    * [style]: 화질 조정
    
    * [style]: 클립보드 복사 구현
    
    * [feat]: Analytics Report API 반영 완료
    
    * [feat]: Analytics Report 그래프 축 변경

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,12 @@ A
 src/Router.js,5/e/5e8ddba814c3eac71a3491f62c12f2a9102f3e77
 A
 src/page/login.js,5/5/5572d9d886c7f97dadbc71a1be778959a0496b8e
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+src/App.js,1/e/1e43fa63aecd2127be034b3fb9d4caa172275714
+A
+package-lock.json,f/a/fa288d1472d29beccb489a676f68739ad365fc47
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
--- src/component/chatList.js
+++ src/component/chatList.js
@@ -13,7 +13,7 @@ import Button from '@mui/material/Button';
 import messagePanel from '../image/messagePanel.png';
 import profile from "../image/profile.png";
 import copyCodeButton from "../image/copyCodeButton.png";
-
+import ClipBoardAlert from "../component/clipBoardAlert.js";
 const ChatList = () => {
   const [value, setValue] = React.useState('1');
 
@@ -29,19 +29,21 @@ const ChatList = () => {
               height="95%"
               // style={{ marginTop: "50px" }}
           />
-          <Button 
+          <Button
           variant='contained'
           style={{
-              position: 'absolute', 
-              top: 190, 
-              right: 1040, 
+              position: 'absolute',
+              top: 190,
+              right: 1040,
               zIndex: 2,
               background : '#7027CD',
-              
+
           }}>
             채팅
           </Button>
-          <Button style={{position: "absolute", bottom:"30px", left:"130px"}}><img style={{width:"300px"}} src={copyCodeButton}/></Button>
+          {/*<Button style={{position: "absolute", bottom:"30px", left:"130px"}}><img style={{width:"300px"}} src={copyCodeButton}/></Button>*/}
+          {/*<Button onClick={() => handleCopyClipBoard('clipBoard')}style={{position: "absolute", bottom:"30px", left:"130px"}}><img style={{width:"300px"}} src={copyCodeButton}/></Button>*/}
+          <ClipBoardAlert/>
       </div>);
 };
 
